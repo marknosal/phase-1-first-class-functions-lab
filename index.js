a856b37ba9d29dba00a5942714ad445e20874fe0
Mark Nosal <mark.c.nosal@gmail.com>
2023-01-15 13:21:31 -0600

    Finished Lab

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 // Code your solution in this file!
-test
+const returnFirstTwoDrivers = (drivers) => drivers.slice(0,2);
+
+const returnLastTwoDrivers = (drivers) => drivers.slice(drivers.length-2, drivers.length);
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (multiplier) => (fare) => multiplier * fare;
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function (drivers, selectedDrivers) {
+    if (selectedDrivers === returnFirstTwoDrivers) {
+        return returnFirstTwoDrivers(drivers);
+    } else if (selectedDrivers === returnLastTwoDrivers) {
+        return returnLastTwoDrivers(drivers);
+    } else {
+        return 'Wrong input';
+    }
+}
